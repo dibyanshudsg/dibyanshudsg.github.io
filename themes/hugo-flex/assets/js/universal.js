6f5602181801efb06a2a99e6a5fefa1512a2eb79
Dibyanshu <dibyanshu@aurosociety.org>
2025-10-03 11:13:10 +0530

    fress push 4

--- themes/hugo-flex/assets/js/universal.js
+++ themes/hugo-flex/assets/js/universal.js
@@ -15,3 +15,26 @@ document.addEventListener("contextmenu", (e) => e.preventDefault());
 // Disable copy & cut
 document.addEventListener("copy", (e) => e.preventDefault());
 document.addEventListener("cut", (e) => e.preventDefault());
+
+// lazy lode
+
+document.addEventListener("DOMContentLoaded", function () {
+  const lazyBackgrounds = document.querySelectorAll(".lazy");
+
+  const lazyLoad = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const bg = entry.target.dataset.bg;
+        entry.target.style.backgroundImage = `url('${bg}')`;
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(lazyLoad, {
+    rootMargin: "100px 0px",
+    threshold: 0.1,
+  });
+
+  lazyBackgrounds.forEach((el) => observer.observe(el));
+});
